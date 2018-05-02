c10443003aa9e6ee33876cd02c94da72b93c1a77
Karliscraft <karlis.aritis2@inbox.lv>
2018-05-02 21:11:52 +0300

    Ugh...

--- main.go
+++ main.go
@@ -1,1 +1,45 @@
+package main
 
+import (
+	"encoding/json"
+	"os"
+	"log"
+	"fmt"
+	"bufio"
+	"os/exec"
+	"io"
+)
+
+func ReadLine(){
+	
+}
+
+func main(){
+	cmd = exec.Command("sxhkd")
+	cmd.Start()
+	stdin, err = cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	messages := make(chan map[string]string, 1)
+	var blocks map[string]string
+	reader := bufio.NewReader(os.Stdin)
+	if _, err := reader.ReadString('\n'); err != nil {
+		log.Fatal(err)
+	}
+	dec := json.NewDecoder(reader)
+	if _, err := dec.Token(); err != nil {
+		log.Fatal(err)
+	}
+	
+	for dec.More() {
+		var block map[string]string
+		if val, ok := block["label"]; ok {
+			blocks[val], _ = block["full_text"]
+			messages <- blocks
+		}
+	}
+	if _, err := dec.Token(); err != nil {
+		log.Fatal(err)
+	}
+}
